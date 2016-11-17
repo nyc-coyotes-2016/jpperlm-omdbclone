2eea564ce872be76621b0354e4cf47cb7381b47f
Jason Perlman <email": null>
2016-11-17 12:23:01 -0500

    Linking to real imdb

--- app/assets/javascripts/components/SingleMovie.es6.jsx
+++ app/assets/javascripts/components/SingleMovie.es6.jsx
@@ -2,9 +2,10 @@ class SingleMovie extends React.Component {
   render(){
     var movie = this.props.data;
     if (movie){
+      var url = "http://www.imdb.com/title/"+ movie.imdbID;
       return (
         <div className='singleMovies'>
-          <h1>{movie.Title}</h1>
+          <a href={url}><h1>{movie.Title}</h1></a>
           <h3>{movie.Actors}</h3>
           <h4>{movie.Genre}</h4>
           <p>Released: {movie.Released}</p>
